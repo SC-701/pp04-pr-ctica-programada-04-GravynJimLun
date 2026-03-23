611a899c532a24182effd9be39c05750857bcb0b
GravynJimenez <gravyn.jimenez.luna@gmail.com>
2026-03-23 12:18:06 -0600

    Primer Commit

---
+++ Vehiculo.API/Abstracciones/Interfaces/Servicios/IRevisionServicio.cs
@@ -0,0 +1,10 @@
+﻿using Abstracciones.Modelos.Servicios.Registro;
+
+namespace Abstracciones.Interfaces.Servicios
+{
+    public interface IRevisionServicio
+    {
+
+        Task<Revision> Obtener(string placa);
+    }
+}
---
+++ Vehiculo.API/Abstracciones/Modelos/Servicios/Registro/Falta.cs
@@ -0,0 +1,9 @@
+﻿namespace Abstracciones.Modelos.Servicios.Registro
+{
+    public class Falta
+    {
+        public string Tipo { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
